ae61392f8f31101caf7a3da3950dda92ddf1df32
Nartallax <nartallax@gmail.com>
2022-09-17 09:13:20 +0400

    Box considered finished

--- src/client/base/box.ts
+++ src/client/base/box.ts
@@ -1,5 +1,3 @@
-import {addPrototypeToFunction} from "client/base/add_prototype_to_function"
-
 type SubscriberHandlerFn<T = unknown> = (value: T) => void
 type UnsubscribeFn = () => void
 
@@ -123,6 +121,17 @@ class BoxNotificationStack {
 			stackTop.add(v as AnyBoxImpl<unknown>)
 		}
 	}
+}
+
+/** Having a function and a prototype, make a function with this prototype and its properties
+ * Unfortunately, this implies that constructor of the parent function cannot be called on the function
+ * because since ES6 classes can't be called like regular functions and there is no known workaround
+ * But any properties defined on the prototype will be copied to the function instance, so it's not too bad
+ * Also the function won't receive proper `this` object. This can be done at the cost of performance; I don't need it that hard */
+function addPrototypeToFunction<R, A extends never[], F extends (this: null, ...args: A) => R, I extends object>(fn: F, obj: I): I & F {
+	Object.setPrototypeOf(fn, Object.getPrototypeOf(obj)) // set up the prototype
+	Object.assign(fn, obj) // clone the properties
+	return fn as I & F
 }
 
 
@@ -678,6 +687,7 @@ class ArrayValueWrapViewBox<T, K> extends ViewBox<ValueBox<T>[]> {
 		let index = -1
 		if(this.haveSubscribers()){
 			// if we are subscribed - we can use index, it is guaranteed to be consistent with the upstream
+			// it is optimisation anyway; if it will cause trouble - we always can just search for the key every time
 			index = box.index
 		} else {
 			for(let i = 0; i < upstreamValue.length; i++){
