15de604576c271ae4731cd9ae17b512bffd5bfaa
Nartallax <nartallax@gmail.com>
2023-06-29 00:16:30 +0400

    Fix zero sized thumbnails request

--- src/client/app/thumbnail_provider.ts
+++ src/client/app/thumbnail_provider.ts
@@ -13,39 +13,42 @@ export class ThumbnailProvider {
 	async getThumbnails(pictures: Picture[]): Promise<HTMLImageElement[]> {
 		const unknownPictures = pictures.filter(pic => !this.urlCache.has(pic.id))
 
-		const encodedBytes = await ClientApi.getPictureThumbnails(unknownPictures)
-		const byteArr = new Uint8Array(encodedBytes)
+		if(unknownPictures.length > 0){
+			const encodedBytes = await ClientApi.getPictureThumbnails(unknownPictures)
+			const byteArr = new Uint8Array(encodedBytes)
 
-		const lengths: number[] = new Array(unknownPictures.length)
-		let offset = 0
-		for(let i = 0; i < unknownPictures.length; i++){
-			const len = (byteArr[offset + 0]! << 0)
+			const lengths: number[] = new Array(unknownPictures.length)
+			let offset = 0
+			for(let i = 0; i < unknownPictures.length; i++){
+				const len = (byteArr[offset + 0]! << 0)
 					| (byteArr[offset + 1]! << 8)!
 					| (byteArr[offset + 2]! << 16)
 					| (byteArr[offset + 3]! << 24)
-			lengths[i] = len
-			offset += 4
-		}
+				lengths[i] = len
+				offset += 4
+			}
 
 
-		for(let i = 0; i < lengths.length; i++){
-			const imgByteLength = lengths[i]!
-			const picture = unknownPictures[i]!
-			const imgBytes = encodedBytes.slice(offset, offset + imgByteLength)
-			offset += imgByteLength
 
-			if(this.urlCache.has(picture.id)){
+			for(let i = 0; i < lengths.length; i++){
+				const imgByteLength = lengths[i]!
+				const picture = unknownPictures[i]!
+				const imgBytes = encodedBytes.slice(offset, offset + imgByteLength)
+				offset += imgByteLength
+
+				if(this.urlCache.has(picture.id)){
 				// for rare cases of concurrent requests for same picture
 				// can't imagine how this could happen
 				// but should take this into account anyway
-				continue
-			}
+					continue
+				}
 
-			const blob = new Blob([imgBytes])
-			// we'll probably never revoke them
-			// oh well. it shouldn't be too much of a problem, thumbs are small
-			const url = URL.createObjectURL(blob)
-			this.urlCache.set(picture.id, url)
+				const blob = new Blob([imgBytes])
+				// we'll probably never revoke them
+				// oh well. it shouldn't be too much of a problem, thumbs are small
+				const url = URL.createObjectURL(blob)
+				this.urlCache.set(picture.id, url)
+			}
 		}
 
 		const imgs: HTMLImageElement[] = pictures.map(picture => tag({tag: "img", attrs: {
