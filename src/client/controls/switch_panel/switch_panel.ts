9f35cd8fc9c03f9eba61254f9d6d830feb9a7a97
Nartallax <nartallax@gmail.com>
2023-09-07 20:32:16 +0400

    Fade-out and fade-in for switchpanels

--- src/client/controls/switch_panel/switch_panel.ts
+++ src/client/controls/switch_panel/switch_panel.ts
@@ -16,8 +16,34 @@ export const SwitchPanel = defineControl(<T extends string>(props: Props<T>) => 
 	}
 
 	bindBox(result, props.value, route => {
-		result.replaceChildren(renderRoute(route))
+		const oldChild = result.lastElementChild as HTMLElement | null
+		const newChild = renderRoute(route)
+		clearOldChild(newChild) // just in case
+
+		if(oldChild){
+			oldChild.classList.remove(css.upcomingSwitchElement!)
+			oldChild.classList.add(css.decayingSwitchElement!)
+			const rect = result.getBoundingClientRect()
+			oldChild.style.width = rect.width + "px"
+			oldChild.style.height = rect.height + "px"
+		}
+		newChild.classList.add(css.upcomingSwitchElement!)
+		result.appendChild(newChild)
+
+		setTimeout(() => {
+			if(oldChild){
+				oldChild.remove()
+				clearOldChild(oldChild)
+			}
+			newChild.classList.remove(css.upcomingSwitchElement!)
+		}, 250)
 	})
 
 	return result
 })
+
+function clearOldChild(oldChild: HTMLElement): void {
+	oldChild.style.height = ""
+	oldChild.style.width = ""
+	oldChild.classList.remove(css.decayingSwitchElement!)
+}
