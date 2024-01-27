8da741f00a9a053001c085610cd81a73a10a8aec
Nartallax <nartallax@gmail.com>
2024-01-27 18:55:40 +0100

    some WIP, don't remember what it is exactly

--- src/client/controls/grid_for_real_this_time/grid.ts
+++ src/client/controls/grid_for_real_this_time/grid.ts
@@ -1,7 +1,7 @@
 import {MRBox, RBox, WBox} from "@nartallax/cardboard"
 import {ClassName, tag} from "@nartallax/cardboard-dom"
 import * as css from "./grid.module.scss"
-import {GridDataRow} from "client/controls/grid_for_real_this_time/grid_internal_types"
+import {GridDataRow, PropKeysThatExtendType} from "client/controls/grid_for_real_this_time/grid_internal_types"
 
 interface CommonProps<T>{
 	/** Get the key of a single element.
@@ -14,14 +14,41 @@ interface CommonProps<T>{
 	}
 }
 
+
+interface PropTreeConfig<T>{
+	/** Name of property that contains children elements.
+	 * Will act as argument for `.prop()` method in box mapping. */
+	readonly children: PropKeysThatExtendType<T, readonly T[]>
+}
+
+interface ImmutableTreeConfig<T> {
+	/** Get array containing children of the row.
+	 * null means this row cannot have children.
+	 *
+	 * It is expected for this function to return a property of an row object, but that's not required.
+	 * New array can be created each time in case you don't store children, and only want grid to fetch them. */
+	getChildren: (element: T) => (readonly T[]) | null
+}
+
+interface MutableTreeConfig<T> extends ImmutableTreeConfig<T> {
+	/** Set children of the row, creating a new row object.
+	 * This function will be used as reverse-mapper in box mapping.
+	 *
+	 * Not required to be passed;
+	 * if not passed, grid will assume you don't store children (but will store them internally) */
+	setChildren?: (element: T, children: readonly T[]) => T
+}
+
 interface ImmutableDataProps<T> extends CommonProps<T> {
 	readonly data: RBox<readonly T[]>
 	readonly renderRow: (element: RBox<T>) => MRBox<string | HTMLElement>
+	readonly tree?: ImmutableTreeConfig<T> | PropTreeConfig<T>
 }
 
 interface MutableDataProps<T> extends CommonProps<T> {
 	readonly data: WBox<readonly T[]>
 	readonly renderRow: (element: WBox<T>) => MRBox<string | HTMLElement>
+	readonly tree?: MutableTreeConfig<T> | PropTreeConfig<T>
 }
 
 export type GridProps<T> = MutableDataProps<T> | ImmutableDataProps<T>
--- src/client/controls/grid_for_real_this_time/grid_internal_types.ts
+++ src/client/controls/grid_for_real_this_time/grid_internal_types.ts
@@ -4,8 +4,16 @@ export interface GridPosition {
 	offset: number
 }
 
+export type PropKeysThatExtendType<BaseObject, PropType, Key extends keyof BaseObject = keyof BaseObject> =
+	Key extends keyof BaseObject // just for distributiveness
+		? BaseObject[Key] extends PropType
+			? Key
+			: never
+		: never
+
 /** A single box from source data, as well as some of the grid's internal data */
 export interface GridDataRow<T> extends GridPosition {
 	readonly box: WBox<T>
+	readonly children: WBox<readonly T[]> | null
 	readonly element: HTMLElement
 }
